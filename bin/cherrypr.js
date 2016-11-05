fb5b38614302749a7a84ff42abbc99a17e2454d3
Zack Story <zack@root-two.com>
2016-11-05 16:49:33 -0700

    cherrypr

--- bin/cherrypr.js
+++ bin/cherrypr.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
-    
-console.log('BIN!')
+
+var execSync = require('child_process').execSync
+
+let lastCommit = execSync(`git rev-parse HEAD`)
+let currentBranch = execSync(`git branch`)
+let newBranch = Math.floor(Math.random()*999999999).toString(36)
+
+console.log('LAST', lastCommit)
+execSync(`git checkout -b ${newBranch} origin/master`, {stdio: [0, 1, 2]})
+execSync(`git cherry-pick ${lastCommit}`, {stdio: [0, 1, 2]})
+execSync(`git push origin ${newBranch}`)
+execSync(`hub pull-request`, {stdio: [0, 1, 2]})
+execSync(`git checkout ${currentBranch}`, {stdio: [0, 1, 2]})
