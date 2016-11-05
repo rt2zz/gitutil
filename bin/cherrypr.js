bae1174fe15e242e1c4ff83254dc8085414fd885
Zack Story <zack@root-two.com>
2016-11-05 16:54:46 -0700

    test2

--- bin/cherrypr.js
+++ bin/cherrypr.js
@@ -6,7 +6,7 @@ let lastCommit = execSync(`git rev-parse HEAD`)
 let currentBranch = execSync(`git branch`)
 let newBranch = Math.floor(Math.random()*999999999).toString(36)
 
-console.log('INFO', lastCommit, currentBranch, newBranch)
+console.log('INFO:', lastCommit, currentBranch, newBranch)
 execSync(`git checkout -b ${newBranch} origin/master`, {stdio: [0, 1, 2]})
 execSync(`git cherry-pick ${lastCommit}`, {stdio: [0, 1, 2]})
 execSync(`git push origin ${newBranch}`)
