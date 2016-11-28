c64e650bb5f6a4dfbf2810c793792ab2a1b9102e
rt2zz <zack@root-two.com>
2016-11-27 16:56:17 -0800

    commit working state

--- bin/smalls-safereset.js
+++ bin/smalls-safereset.js
@@ -3,11 +3,9 @@
 const execSync = require('child_process').execSync
 const name = require('./utils/name')
 
-module.exports = (arg) => {
-  console.log('arg', arg)
-  execSync(`git fetch origin`, {stdio: [0, 1, 2]})
-  execSync(`git add . -v`, {stdio: [0, 1, 2]})
-  execSync(`git commit --allow-empty -m "commit working state"`, {stdio: [0, 1, 2]})
-  execSync(`git branch ${name.backupBranch()}`, {stdio: [0, 1, 2]})
-  execSync(`git reset --hard origin/master`, {stdio: [0, 1, 2]})
-}
+console.log('arg')
+execSync(`git fetch origin`, {stdio: [0, 1, 2]})
+execSync(`git add . -v`, {stdio: [0, 1, 2]})
+execSync(`git commit --allow-empty -m "commit working state"`, {stdio: [0, 1, 2]})
+execSync(`git branch ${name.backupBranch()}`, {stdio: [0, 1, 2]})
+execSync(`git reset --hard origin/master`, {stdio: [0, 1, 2]})
--- bin/smalls.js
+++ bin/smalls.js
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
 
-const pkg =  require('./package.json')
+const pkg =  require('../package.json')
 const program = require('commander')
 
 const app = program
   .version(pkg.version)
   .command('cherrypr', 'Make and PR a new branch which is origin/master + latest commit of the active branch')
   .command('safereset [name]', 'Reset hard to master, after committing and saving the latest into a new backup branch')
-
-app.parse(process.argv)
-
-// Error check for bad commands
-const commandExists = app.commands.some(cmd => cmd._name == app.args[0])
-if (app.args[0] && !commandExists) {
-  throw new Error('command not found')
-  app.outputHelp()
-}
+  .parse(process.argv)
+  
